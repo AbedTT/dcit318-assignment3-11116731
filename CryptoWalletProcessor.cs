76364ac1876bbd971c0e51ef839cef5e6abf9e48
AbedTT <tettehabed19@gmail.com>
2025-08-15 08:38:39 +0000

    Delete CryptoWalletProcessor.cs

--- CryptoWalletProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class CryptoWalletProcessor : ITransactionProcessor
-{
-    public void Process(Transaction transaction)
-    {
-        Console.WriteLine($"Processing Crypto Wallet transaction of {transaction.Amount:C} under category: {transaction.Category}");
-    }
-}
