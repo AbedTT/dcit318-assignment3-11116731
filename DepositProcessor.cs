c938714caf35971a74bf29389edc0badbcff46ba
AbedTT <tettehabed19@gmail.com>
2025-08-15 08:38:19 +0000

    Delete DepositProcessor.cs

--- DepositProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class DepositProcessor : ITransactionProcessor
-{
-    public void Process(Transaction transaction)
-    {
-        Console.WriteLine($"Processing Deposit of {transaction.Amount:C} into account.");
-    }
-}
